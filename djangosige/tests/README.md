d59f6b3f6e468cdac53b0bb89b5ebe2cefb23319
thiagopena <thiagovilelap@hotmail.com>
2017-07-05 18:27:29 -0300

    Criando diretorio unico de testes

---
+++ djangosige/tests/README.md
@@ -0,0 +1,7 @@
+# Testes
+
+- Carregar dados iniciais de teste:
+
+    ```bash
+    python manage.py loaddata djangosige/tests/test_db_backup.json
+    ```
--- djangosige/tests/cadastro/test_views.py
+++ djangosige/tests/cadastro/test_views.py
@@ -1,8 +1,9 @@
 from django.test import TestCase, Client
 from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
 
 
-class AdicionarClienteTestCase(TestCase):
+class AdicionarClienteTestCase(TestCase):        
     def test_adicionar_cliente_resolves(self):
         self.url = reverse('cadastro:addclienteview')
         self.client = Client()
