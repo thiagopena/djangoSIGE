1e821f845e3dc07ab06d2312cf8dc98ac9da0220
wfoschiera <wfoschiera@gmail.com>
2021-06-01 14:47:56 -0400

    change default_database_url var to .env file

--- djangosige/configs/configs.py
+++ djangosige/configs/configs.py
@@ -1,9 +1,3 @@
-# Configuração da base de dados
-# Exemplo: DEFAULT_DATABASE_URL = 'postgres://user:pass@localhost/dbname'
-# Caso seja deixado vazio o default será: 'sqlite:////...djangosige/db.sqlite3'
-DEFAULT_DATABASE_URL = ''
-
-#
 # Configurações do servidor de email
 # Obs: Por enquanto o endereço de email é utilizado apenas para a troca de senha do usuário.
 # Endereço de email padrão utilizado
