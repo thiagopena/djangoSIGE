02f0b5bf4cd9c3a722805e5eb650208f18de949e
Danilo Marto de Carvalho <dmcarvalho@tutamail.com>
2023-03-25 13:32:57 -0300

    O módulo config/settings.py foi inteiramente reescrito

--- djangosige-api/config/settings.py
+++ djangosige-api/config/settings.py
@@ -1,147 +1,162 @@
-import os
-from decouple import config, Csv
+"""Defnições do projeto"""
+
+from pathlib import Path
+
+from decouple import Csv, UndefinedValueError, config
 from dj_database_url import parse as dburl
-from .configs import DEFAULT_DATABASE_URL, DEFAULT_FROM_EMAIL, EMAIL_HOST, EMAIL_HOST_USER, EMAIL_HOST_PASSWORD, EMAIL_PORT, EMAIL_USE_TLS
 
-APP_ROOT = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-PROJECT_ROOT = os.path.abspath(os.path.dirname(APP_ROOT))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
-
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
-
-if not DEFAULT_DATABASE_URL:
-    DEFAULT_DATABASE_URL = 'sqlite:///' + os.path.join(APP_ROOT, 'db.sqlite3')
-
-DATABASES = {
-    'default': config('DATABASE_URL', default=DEFAULT_DATABASE_URL, cast=dburl),
-}
+APP_ROOT = BASE_DIR / "djangosige"
 
 
-# Application definition
+# === ... === #
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv())
 
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+# === Sessão === #
+SESSION_EXPIRE_AT_BROWSER_CLOSE = False
 
-    # djangosige apps:
-    'djangosige.apps.base',
-    'djangosige.apps.login',
-    'djangosige.apps.cadastro',
-    'djangosige.apps.vendas',
-    'djangosige.apps.compras',
-    'djangosige.apps.fiscal',
-    'djangosige.apps.financeiro',
-    'djangosige.apps.estoque',
-]
+# === Banco de Dados === #
+try:
+    DATABASES = {
+        "default": config("DATABASE_URL", cast=dburl),
+    }
+except (UndefinedValueError, KeyError):
+    DATABASES = {
+        "default": {
+            "ENGINE": config("SQL_ENGINE"),
+            "NAME": config("SQL_NAME"),
+            "USER": config("SQL_USER"),
+            "PASSWORD": config("SQL_PASSWORD"),
+            "HOST": config("SQL_HOST"),
+            "PORT": config("SQL_PORT"),
+        }
+    }
 
+# === Debugging === #
+DEBUG = config("DEBUG", cast=bool)
 
+# === E-mail === #
+DEFAULT_FROM_EMAIL = "webmaster@localhost"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = config("EMAIL_HOST")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_PORT = config("EMAIL_PORT")
+EMAIL_USE_TLS = config("EMAIL_USE_TLS")
+EMAIL_USE_SSL = config("EMAIL_USE_SSL")
+
+# === Uploads de arquivos === #
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "mediafile/"
+
+# === Internacionalização === #
+LANGUAGE_CODE = "pt-BR"
+TIME_ZONE = "America/Sao_Paulo"
+USE_I18N = True
+USE_TZ = True
+USE_TZ = True
+
+# === HTTP === #
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-    # Middleware para paginas que exigem login
-    'djangosige.middleware.LoginRequiredMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "djangosige.middleware.LoginRequiredMiddleware",
 ]
+WSGI_APPLICATION = "config.wsgi.application"
 
-ROOT_URLCONF = 'djangosige.urls'
+# === Modelos === #
+FIXTURE_DIRS = [
+    APP_ROOT / "fixtures",
+]
+DJANGO_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+]
+THIRD_APPS = []
+LOCAL_APPS = [
+    "djangosige.apps.base",
+    "djangosige.apps.login",
+    "djangosige.apps.cadastro",
+    "djangosige.apps.vendas",
+    "djangosige.apps.compras",
+    "djangosige.apps.fiscal",
+    "djangosige.apps.financeiro",
+    "djangosige.apps.estoque",
+]
+INSTALLED_APPS = DJANGO_APPS + THIRD_APPS + LOCAL_APPS
 
+# === Segurança === #
+SECRET_KEY = config("SECRET_KEY")
+
+# === Templates === #
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(APP_ROOT, 'templates'), ],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                # contexto para a versao do sige
-                'djangosige.apps.base.context_version.sige_version',
-                # contexto para a foto de perfil do usuario
-                'djangosige.apps.login.context_user.foto_usuario',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+            APP_ROOT / "templates",
+        ],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "djangosige.apps.base.context_version.sige_version",
+                "djangosige.apps.login.context_user.foto_usuario",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'djangosige.wsgi.application'
+# === URLs === $
+ROOT_URLCONF = "config.urls"
 
-
-# Password validation
-# https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
-
+# === Auth === #
+AUTH_USER_MODEL = "auth.User"
+LOGIN_REDIRECT_URL = "/"
+LOGIN_URL = "/login/"
+LOGOUT_REDIRECT_URL = "/login/"
+LOGIN_NOT_REQUIRED = (
+    r"^/login/$",
+    r"/login/esqueceu/",
+    r"/login/trocarsenha/",
+    r"/logout/",
+)
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
+    "django.contrib.auth.hashers.Argon2PasswordHasher",
+    "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
+]
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",  # noqa
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.10/topics/i18n/
-
-#LANGUAGE_CODE = 'en-us'
-LANGUAGE_CODE = 'pt-br'
-
-#TIME_ZONE = 'UTC'
-TIME_ZONE = 'America/Sao_Paulo'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.10/howto/static-files/
-
-STATIC_URL = '/static/'
-
+# === Arquivos Estpaticos === #
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
-    os.path.join(APP_ROOT, 'static'),
+    APP_ROOT / "static",
 ]
-
-FIXTURE_DIRS = [
-    os.path.join(APP_ROOT, 'fixtures'),
-]
-
-MEDIA_ROOT = os.path.join(APP_ROOT, 'media/')
-MEDIA_URL = 'media/'
-
-SESSION_EXPIRE_AT_BROWSER_CLOSE = False
-
-LOGIN_NOT_REQUIRED = (
-    r'^/login/$',
-    r'/login/esqueceu/',
-    r'/login/trocarsenha/',
-    r'/logout/',
-)
