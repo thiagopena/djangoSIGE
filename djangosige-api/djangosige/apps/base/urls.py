291f594b0c359e584b524e4e23d8eeed87ae531e
Danilo Marto de Carvalho <dmcarvalho@tutamail.com>
2023-03-25 13:40:45 -0300

    Os módulos que leem o módulo djangosige.configs.settings (descontinuado) foram modificados para lerem o módulo django.conf.settings

--- djangosige-api/djangosige/apps/base/urls.py
+++ djangosige-api/djangosige/apps/base/urls.py
@@ -1,15 +1,15 @@
+from django.conf import settings
 from django.urls import re_path
+
 from . import views
 
-from djangosige.configs import DEBUG
-
-app_name = 'base'
+app_name = "base"
 urlpatterns = [
-    re_path(r'^$', views.IndexView.as_view(), name='index'),
+    re_path(r"^$", views.IndexView.as_view(), name="index"),
 ]
 
-if DEBUG:
+if settings.DEBUG:
     urlpatterns += [
-        re_path(r'^404/$', views.handler404),
-        re_path(r'^500/$', views.handler500),
+        re_path(r"^404/$", views.handler404),
+        re_path(r"^500/$", views.handler500),
     ]
