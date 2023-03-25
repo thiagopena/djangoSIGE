99ba6d3dc82feaefb2f1c2071af48cbb2b3442f2
Danilo Marto de Carvalho <dmcarvalho@tutamail.com>
2023-03-25 17:05:23 -0300

    Removi do projeto as rotas que não dizem respeito a API

--- djangosige-api/config/urls.py
+++ djangosige-api/config/urls.py
@@ -8,6 +8,7 @@ from django.urls import include, re_path
 from djangosige.contrib.swagger.views import schema_view
 
 urlpatterns = [
+    re_path(r"^admin/", admin.site.urls),
     re_path(
         r"^swagger(?P<format>\.json|\.yaml)$",
         schema_view.without_ui(cache_timeout=0),
@@ -23,20 +24,4 @@ urlpatterns = [
         schema_view.with_ui("redoc", cache_timeout=0),
         name="schema-redoc",
     ),
-    re_path(r"^admin/", admin.site.urls),
-    re_path(r"^", include("djangosige.apps.base.urls")),
-    re_path(r"^login/", include("djangosige.apps.login.urls")),
-    re_path(r"^cadastro/", include("djangosige.apps.cadastro.urls")),
-    re_path(r"^fiscal/", include("djangosige.apps.fiscal.urls")),
-    re_path(r"^vendas/", include("djangosige.apps.vendas.urls")),
-    re_path(r"^compras/", include("djangosige.apps.compras.urls")),
-    re_path(r"^financeiro/", include("djangosige.apps.financeiro.urls")),
-    re_path(r"^estoque/", include("djangosige.apps.estoque.urls")),
 ]
-
-if settings.DEBUG:
-    urlpatterns = (
-        urlpatterns
-        + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-        + static(settings.STATIC_URL, document_root=settings.STATIC_URL)
-    )
