df728510a9c08316bdd26569ded8f30c99f6f358
Danilo Marto de Carvalho <dmcarvalho@tutamail.com>
2023-03-25 12:35:04 -0300

    Atualizei o utilitário manage.py para ler o módulo config.settings.py

--- djangosige-api/manage.py
+++ djangosige-api/manage.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
+
 import os
-import sys
 
 if __name__ == "__main__":
-    settings = 'djangosige.tests.test_settings' if 'test' in sys.argv else 'djangosige.configs'
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
